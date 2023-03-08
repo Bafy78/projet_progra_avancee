98ff9535a4832313efd0f7f598bc7cd88d9bc3f2
Thuthur22 <102926386+Thuthur22@users.noreply.github.com>
2023-03-08 15:23:02 +0100

    Première version evaluateur avec test (#1)
    * Première version évaluateur
    
    * première version test evaluateur
    
    ---------
    
    Co-authored-by: Bafy78 <30290088+Bafy78@users.noreply.github.com>

--- evaluateur.c
+++ evaluateur.c
@@ -1,5 +1,79 @@
+#include <math.h>
+#include <stdlib.h>
+#include "jeton.h"
 #include "evaluateur.h"
 
-int fonction3(){
-    return 0;
+//retourne l'image de x par la fonction représentée par l'arbre A
+float Eval( Arbre A,int x,int *erreur_pg){
+    //la fonction est récursive, donc on ne continue que si il n'y a pas eu d'erreur à la récursion précédente
+	if (erreur_pg==0){
+        //Pour chaque type de lexem, on effectue l'opération associée, avec les éléments du tableau concernés
+		switch(A->jeton.lexem){
+			case REEL :
+				return((float)(A->jeton.valeur.reel));
+				break;
+			case VARIABLE :
+				return(x);
+				break;
+			case OPERATEUR:
+				switch(A->jeton.valeur.operateur){
+                    //Par exemple si on a un opérateur plus, on additione les élément du fils gauche avec ceux du fils droit
+					case PLUS:
+						return(Eval(A->fg,x,erreur_pg)+Eval(A->fd,x,erreur_pg));
+						break;
+					case MOINS:
+						return(Eval(A->fg,x,erreur_pg)-Eval(A->fd,x,erreur_pg));
+						break;
+					case FOIS:
+						return(Eval(A->fg,x,erreur_pg)*Eval(A->fd,x,erreur_pg));
+						break;
+					case DIV:
+						return(Eval(A->fg,x,erreur_pg)/Eval(A->fd,x,erreur_pg));
+						break;
+					case PUIS:
+						return(pow(Eval(A->fg,x,erreur_pg),Eval(A->fd,x,erreur_pg)));
+						break;
+					
+				}					
+			case FONCTION:
+				switch(A->jeton.valeur.fonction){
+                    //Pour les fonctions comme sinus, on effectue la fonction sur les éléments du fils droit
+					case SIN:
+						return (sin (Eval(A->fd,x,erreur_pg)));
+						break;
+					case ABS:
+						return (abs(Eval(A->fd,x,erreur_pg)));
+						break;
+					case SQRT:
+						return (sqrt(Eval(A->fd,x,erreur_pg)));
+						break;
+					case LOG:
+						return (log(Eval(A->fd,x,erreur_pg)));
+						break;
+					case COS:
+						return (cos(Eval(A->fd,x,erreur_pg)));
+						break;
+					case TAN:
+						return (tan(Eval(A->fd,x,erreur_pg)));
+						break;
+					case EXP:
+						return (exp(Eval(A->fd,x,erreur_pg)));
+						break;
+					case ENTIER:
+						return ((int)(floor(Eval(A->fd,x,erreur_pg))));
+						break;
+					case VAL_NEG:
+						return (-1*(Eval(A->fd,x,erreur_pg)));
+						break;
+					case SINC:
+						return (sin(Eval(A->fd,x,erreur_pg))/Eval(A->fd,x,erreur_pg));
+						break;
+					
+				}
+            //Si on ne retrouve aucun des lexems précédents, il y a une erreur
+			default:
+				*erreur_pg=301; //lexem non reconnu		
+		}
+	}
+	return 0.0;
 }
--- main_test_evaluateur.c
+++ main_test_evaluateur.c
@@ -1,5 +1,83 @@
+#include <math.h>
+#include <stdio.h>
+#include "jeton.h"
 #include "evaluateur.h"
 
+Arbre creer_noeud(typejeton T,Arbre fg,Arbre fd){
+    Arbre New;
+    New=(Arbre)malloc(sizeof(Node));
+    New->jeton=T;
+    New->fg=fg;
+    New->fd=fd;
+    return New;
+}
+Arbre generation_arbre(){
+    typejeton jetonplus;
+    jetonplus.lexem=OPERATEUR;
+    jetonplus.valeur.operateur=PLUS;
+
+    typejeton jetonfois;
+    jetonfois.lexem=OPERATEUR;
+    jetonfois.valeur.operateur=FOIS;
+
+    typejeton jetondiv;
+    jetondiv.lexem=OPERATEUR;
+    jetondiv.valeur.operateur=DIV;
+
+    typejeton jetonvar;
+    jetonvar.lexem=VARIABLE;
+
+    typejeton jetonvalneg;
+    jetonvalneg.lexem=FONCTION;
+    jetonvalneg.valeur.fonction=VAL_NEG;
+
+    typejeton jetoncinq;
+    jetoncinq.lexem=REEL;
+    jetoncinq.valeur.reel=5;
+
+    typejeton jetontrois;
+    jetontrois.lexem=REEL;
+    jetontrois.valeur.reel=3;
+
+    typejeton jetoncos;
+    jetoncos.lexem=FONCTION;
+    jetoncos.valeur.fonction=COS;
+
+    Arbre A= creer_noeud(
+        jetonplus,
+        creer_noeud(
+            jetonfois,
+            creer_noeud(
+                jetonvalneg,NULL,
+                creer_noeud(
+                    jetoncinq,NULL,NULL
+                )
+
+            ),
+            creer_noeud(
+                jetonvar, NULL , NULL
+            )
+        ),
+        creer_noeud(
+            jetoncos,NULL,
+            creer_noeud(
+                jetondiv,
+                creer_noeud(
+                    jetontrois, NULL, NULL
+                ),
+                creer_noeud(
+                    jetonvar, NULL, NULL
+                )
+            )
+        )
+    );
+    return A;
+}
+
+
+
 int main(){
-    return fonction3();
+  Arbre A=generation_arbre();
+  printf("%lf",A->jeton.valeur.reel);
+  printf("le resultat est %f\n", Eval(A,1,0));
 }
