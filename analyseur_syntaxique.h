e33ef9db5681d627a429ec81b459fec31b61b5c7
thpcap <126089066+thpcap@users.noreply.github.com>
2023-03-15 15:04:50 +0100

    debbuged

--- analyseur_syntaxique.h
+++ analyseur_syntaxique.h
@@ -1,1 +1,13 @@
-int fonction2();
+#ifndef analyseur_syntaxique_h
+#define analyseur_syntaxique_h
+
+#include "jeton.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Arbre analyse_syntaxique( typejeton Tab[],int* erreur_pg);
+
+Arbre creer_noeud(typejeton T,Arbre fg,Arbre fd);
+
+#endif
