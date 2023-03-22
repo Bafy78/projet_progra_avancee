11ec1e5854f20b61bb3db94808bd182ee8d70e5c
Bafy78 <30290088+Bafy78@users.noreply.github.com>
2023-03-22 13:53:13 +0100

    Add files via upload

--- analyseur_lexical.c
+++ analyseur_lexical.c
@@ -1,226 +1,157 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-#include "jeton.h"
+#include <ctype.h>
+#include "analyseur_lexical.h"
 
-// Récupère le jeton suivant dans l'entrée
-typejeton get_next_token(const char **input, typejeton prev_jeton)
-{
-    typejeton token;
-    token.lexem = NO_TOKEN;
 
-    // Si l'entrée est vide, retourne un jeton sans type
-    if (!*input)
-    {
-        return token;
-    }
-    // Ignorer les espaces
-    while (isspace(**input))
-    {
-        (*input)++;
-    }
-
-    // Lecture du caractère suivant
-    char c = **input;
-
-    // Détection des lexèmes
-    if (isdigit(c) || c == '.')
-    {
-        // Extrait le nb REEL
-        token.lexem = REEL;
-        sscanf(*input, "%f", &token.valeur.reel);
-        while (isdigit(**input) || **input == '.')
-        {
-            (*input)++;
+void analyse_lexicale(typejeton tab[], int *erreur_pg, char c[]){
+    char ch3[3];
+    char ch4[4];
+    char ch6[6];
+    c="sin(x+3)";
+    int j=0,l=0;
+    printf("passage dans ana lexixal \n");
+    for(int i=0;i<strlen(c);i++){
+        printf("%d :",i);
+        if(i==0 && c[i]=='-'){
+            tab[j].lexem=FONCTION;
+            tab[j].valeur.fonction=VAL_NEG;
+            j++;
+            printf("valneg detecte \n");
         }
-    }
-    else if (isalpha(c))
-    {
-        // FONCTION ou VARIABLE
-        if (c == 'x' || c == 'X')
-        {
-            token.lexem = VARIABLE;
-            (*input)++;
+        if ((c[i])==' '){
+            continue;
         }
-        else
-        {
-            token.lexem = FONCTION;
-            int found = 0;
-
-            // Comparaison avec les noms de fonctions supportées
-            const char *fonctions[] = {"abs", "sin", "sqrt", "log", "cos", "tan", "exp", "entier", "val_neg", "sinc"};
-            for (int i = 0; i < sizeof(fonctions) / sizeof(fonctions[0]); i++)
-            {
-                int len = strlen(fonctions[i]);
-                if (strncmp(*input, fonctions[i], len) == 0)
-                {
-                    token.valeur.fonction = (typefonction)i;
-                    *input += len;
-                    found = 1;
-                    break;
+        else if (c[i]=='+'){
+            tab[j].lexem=OPERATEUR;
+            tab[j].valeur.operateur=PLUS;
+            j++;
+            printf("+ detecte \n");
+        }
+        else if (c[i]=='-'){
+            tab[j].lexem=OPERATEUR;
+            tab[j].valeur.operateur=MOINS;
+            j++;
+            printf("- detecte \n");
+        }
+        else if (c[i]=='*'){
+            tab[j].lexem=OPERATEUR;
+            tab[j].valeur.operateur=FOIS;
+            j++;
+            printf("* detecte \n");
+        }
+        else if (c[i]=='/'){
+            tab[j].lexem=OPERATEUR;
+            tab[j].valeur.operateur=DIV;
+            j++;
+            printf("/ detecte \n");
+        }
+        else if (c[i]=='^'){
+            tab[j].lexem=OPERATEUR;
+            tab[j].valeur.operateur=PUIS;
+            j++;
+            printf("^ detecte \n");
+        }  
+        else if (isdigit(c[i])){
+            tab[j].lexem=REEL;
+            tab[j].valeur.reel=c[i];
+            j++;
+            printf("reel detecte \n");
+        }
+        else if(c[i]=='x'){
+            tab[j].lexem=VARIABLE;
+            j++;
+            printf("variable detecte \n");
+        }
+        else if(isalpha(c[i])){
+            l=0;
+            if(i>=2){
+                for(int k=i-2;k<=i;k++){
+                    ch3[l]=c[k];
+                    l++;
+                    if(strcmp(ch3,"ABS")==0){
+                        tab[j].valeur.fonction=ABS;
+                        j++;
+                        printf("abs detecte \n");
+                    }
+                    else if (strcmp(ch3,"SIN")==0){
+                        tab[j].valeur.fonction=SIN;
+                        j++;
+                        printf("sin detecte \n");
+                    }
+                    else if (strcmp(ch3,"LOG")==0){
+                        tab[j].valeur.fonction=LOG;
+                        j++;
+                        printf("log detecte \n");
+                    }
+                    else if(strcmp(ch3,"COS")==0){
+                        tab[j].valeur.fonction=COS;
+                        j++;
+                        printf("cos detecte \n");
+                    }
+                    else if (strcmp(ch3,"TAN")==0){
+                        tab[j].valeur.fonction=TAN;
+                        j++;
+                        printf("tan detecte \n");
+                    }
+                    else if (strcmp(ch3,"EXP")==0){
+                        tab[j].valeur.fonction=EXP;
+                        j++;
+                        printf("tan detecte \n");
+                    }
                 }
             }
-
-            // Si aucune fonction trouvée, retourne un jeton d'erreur
-            if (!found)
-            {
-                token.lexem = ERREUR;
-                token.valeur.erreur = 101; // Code d'erreur pour fonction inconnue
+            if(i>=3){
+                for(int k=i-3;k<=i;k++){
+                    ch4[l]=c[k];
+                    l++;
+                    if (strcmp(ch4,"SQRT")==0){
+                        tab[j].valeur.fonction=SQRT;
+                        j++;
+                        printf("sqrt detecte \n");
+                    }
+                    else if (strcmp(ch4,"SINC")==0){
+                        tab[j].valeur.fonction=SINC;
+                        j++;
+                        printf("sinc detecte \n");
+                    }
+                }
+            }
+            if(i>=5){
+                for(int k=i-5;k<=i;k++){
+                    ch6[l]=c[k];
+                    l++;
+                    if (strcmp(ch6,"ENTIER")==0){
+                        tab[j].valeur.fonction=ENTIER;
+                        j++;
+                        printf("fonc entier detecte \n");
+                    }
+                }
             }
         }
-    }
-    else
-    {
-        // Autres caractères
-        switch (c)
-        {
-        case '+':
-            token.lexem = OPERATEUR;
-            token.valeur.operateur = PLUS;
-            (*input)++;
-            break;
-        case '-':
-            // Si '-' est un signe moins ou une fonction pour une valeur négative
-            if (prev_jeton.lexem == NO_TOKEN || prev_jeton.lexem == PAR_OUV || prev_jeton.lexem == ABSOLU)
-            {
-                token.lexem = FONCTION;
-
-                token.valeur.fonction = VAL_NEG;
-            }
-            else
-            {
-
-                token.lexem = OPERATEUR;
-
-                token.valeur.operateur = MOINS;
-            }
-            (*input)++;
-            break;
-        case '*':
-            token.lexem = OPERATEUR;
-            token.valeur.operateur = FOIS;
-            (*input)++;
-            break;
-        case '/':
-            token.lexem = OPERATEUR;
-            token.valeur.operateur = DIV;
-            (*input)++;
-            break;
-        case '^':
-            token.lexem = OPERATEUR;
-            token.valeur.operateur = PUIS;
-            (*input)++;
-            break;
-        case '(':
-            token.lexem = PAR_OUV;
-            (*input)++;
-            break;
-        case ')':
-            token.lexem = PAR_FERM;
-            (*input)++;
-            break;
-        case '|':
-            /*       if ((*input)[1] == '|')
-                  { */
-            token.lexem = ABSOLU;
-            (*input)++;
-
-            /*             else
-                        {
-                            token.lexem = ERREUR;
-                            token.valeur.erreur = 102; // Code d'erreur pour caractère inconnu
-                        } */
-            break;
-        case '\0':
-            token.lexem = FIN;
-            break;
-        default:
-            token.lexem = ERREUR;
-            token.valeur.erreur = 102; // Code d'erreur pour caractère inconnu
-            (*input)++;
-            break;
+        else if(c[i]=='('){
+            tab[j].lexem=PAR_OUV;
+            j++;
+            printf("( detecte \n");
         }
-    }
-
-    return token;
-}
-
-// Fonction qui prend une expression et retourne un tableau de jetons
-typejeton *analyseur(const char *expression)
-
-{
-    typejeton static result[length_Tab];
-    typejeton prev_jeton;
-    prev_jeton.lexem = NO_TOKEN;
-    // const char *expression = "(-5cos(-x))";
-    int i = 0;
-
-    const char *input = expression;
-
-    typejeton token;
-    typejeton inserted_times;
-
-    // Ajoutez ces tableaux pour stocker les noms des opérateurs et des fonctions
-    const char *operateur_names[] = {"PLUS", "MOINS", "FOIS", "DIV", "PUIS"};
-    const char *fonction_names[] = {"ABS", "SIN", "SQRT", "LOG", "COS", "TAN", "EXP", "ENTIER", "VAL_NEG", "SINC"};
-
-    do
-    {
-        token = get_next_token(&input, prev_jeton);
-        // printf("%d\n", prev_jeton.lexem);
-
-        if ((prev_jeton.lexem == REEL || prev_jeton.lexem == VARIABLE) &&
-            (token.lexem == FONCTION || token.lexem == PAR_OUV || token.lexem == VARIABLE))
-        {
-            result[i].lexem = OPERATEUR;
-            result[i].valeur.operateur = FOIS;
-            printf("OPERATEUR: %s\n", operateur_names[result[i].valeur.operateur]);
-
-            i++;
+        else if(c[i]==')'){
+            tab[j].lexem=PAR_FERM;
+            j++;
+            printf(") detecte \n");
         }
-        result[i] = token;
-
-        // Affichage des jetons pour le débogage
-        switch (token.lexem)
-        {
-        case REEL:
-            printf("REEL: %f\n", token.valeur.reel);
-            break;
-        case OPERATEUR:
-            printf("OPERATEUR: %s\n", operateur_names[token.valeur.operateur]);
-            break;
-        case FONCTION:
-            printf("FONCTION: %s\n", fonction_names[token.valeur.fonction]);
-            break;
-        case ERREUR:
-            printf("ERREUR: %d\n", token.valeur.erreur);
-            break;
-        case FIN:
-            printf("FIN\n");
-            break;
-        case PAR_OUV:
-            printf("PAR_OUVR\n");
-            break;
-        case PAR_FERM:
-            printf("PAR_FERM\n");
-            break;
-        case VARIABLE:
-            printf("VARIABLE\n");
-            break;
-        case ABSOLU:
-            printf("ABSOLU\n");
-            break;
-        default:
-            printf("NO_TOKEN\n");
-            break;
+        else if(c[i]=='|'){
+            tab[j].lexem=BAR;
+            j++;
+            printf("| detecte \n");
         }
-        i++;
-        prev_jeton = token;
-
-    } while (token.lexem != FIN);
-    printf("===================\n");
-    printf("NB of token %d \n", i);
-
-    return result;
+        else if(c[i]=='\0'){
+            tab[j].lexem=FIN;
+            j++;
+            printf("fin detecte \n");
+        }
+            
+      else{
+          *erreur_pg=100;
+      }
+   }
 }
