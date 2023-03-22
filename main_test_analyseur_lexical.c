b39bb2a5f715797da79abb24f470723c59e22c7b
Bafy78 <30290088+Bafy78@users.noreply.github.com>
2023-03-22 14:00:25 +0100

    Add files via upload

--- main_test_analyseur_lexical.c
+++ main_test_analyseur_lexical.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "analyseur_lexical.h"
+#include "jeton.h"
 
-int main(){
-    return fonction1();
+
+int main()
+{
+    const char *expression = "|-4xcos(5X)|";
+    typejeton *result = analyseur(expression);
+    for (int i = 0; i < 14; i++)
+    {
+        printf("i = %d, lexem : %d, fonction :  %d, operateur : %d, reel : %f \n\n", i, result[i].lexem, result[i].valeur.fonction, result[i].valeur.operateur, result[i].valeur.reel);
+    }
+    return 0;
 }
